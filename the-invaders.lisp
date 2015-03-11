c02eaa184801282bac9acff0c49c4e7b5c798e53
Jan Tatham <jan@sebity.com>
2015-03-11 11:37:57 +0000

    Added: license info

--- the-invaders.lisp
+++ the-invaders.lisp
@@ -1,4 +1,22 @@
-;;;; the-invaders.lisp
+;;;; the-invaders.lisp --- A remake of the glassic game Space Invaders
+
+;; Copyright (C) 2015 Jan Tatham
+
+;; Author: Jan Tatham <jan@sebity.com>
+;; Keywords: games
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 (in-package #:the-invaders)
 
